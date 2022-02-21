935079db0373eff19cd50d9c1bc2f590bd3c26c6
AMAGAT Thibault <amagat.thibault@gmail.com>
2022-02-21 18:26:21 +0100

    meteo

--- src/views/Screens.vue
+++ src/views/Screens.vue
@@ -6,6 +6,7 @@
 
 <script>
 import { marked } from "marked";
+
 export default {
   data() {
     return {
@@ -18,6 +19,41 @@ export default {
     };
   },
   methods: {
+    meteo(inputVal, apiKey) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+      console.log("methode meteo");
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const { main, name, sys, weather } = data;
+          const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+
+          const div = document.createElement("div");
+          div.classList.add("city");
+
+          const markup = `
+                <h2 class="city-name" data-name="${name},${sys.country}">
+                  <span>${name}</span>
+                  <sup>${sys.country}</sup>
+                </h2>
+                <div class="city-temp">${Math.round(
+                  main.temp
+                )}<sup>°C</sup></div>
+                <figure>
+                  <img class="city-icon" src="${icon}" alt="${
+            weather[0]["description"]
+          }">
+                  <figcaption>${weather[0]["description"]}</figcaption>
+                </figure>
+              `;
+
+          div.innerHTML = markup;
+          document.querySelector(".container").appendChild(div);
+        })
+        .catch(() => {
+          alert("erreur la ville n'existe pas ");
+        });
+    },
     logout() {
       this.$router.push("/TokenSelect");
     },
@@ -38,7 +74,7 @@ export default {
         .then((data) => {
           data.data.map((value, key) => {
             this.Boucle = value.Boucle;
-            console.log(`Valeur id : .${value.id}`);
+            //console.log(`Valeur id : .${value.id}`);
             fetch(
               `https://api.interdisp.valentinbardet.dev/items/Sequence_Ecrans?filter[Sequence_id][_eq]=${value.id}&fields=Ecrans_id.Nombre_de_colonnes,Ecrans_id.Nombre_de_lignes,Ecrans_id.Nombre_de_colonnes_video,Ecrans_id.Nombre_de_lignes_video,Ecrans_id.Nombre_de_colonnes_image,Ecrans_id.Nombre_de_lignes_image,Ecrans_id.Type,Ecrans_id.FontColor,Ecrans_id.BackgroundColor,Ecrans_id.Markdown,Ecrans_id.Image,Ecrans_id.Video,Ordre,Duree`
             )
@@ -85,16 +121,17 @@ export default {
 
                                         Sorry, your browser doesn't support embedded videos.
                                     </video>
-                                  </div>`;
+                                  </div>
+                                  `;
             }
           } else {
-            if (this.current.Ecrans_id.Image != null) {
+            if (this.current.Ecrans_id.Type == "image") {
               this.compiled += `<img id='imgFull' src='https://api.interdisp.valentinbardet.dev/assets/${this.current.Ecrans_id.Image}' />`;
-            } else if (this.current.Ecrans_id.Markdown != null) {
+            } else if (this.current.Ecrans_id.Type == "texte") {
               this.compiled += "<section class='texte'>";
               this.compiled += marked.parse(this.current.Ecrans_id.Markdown);
               this.compiled += "</section>";
-            } else if (this.current.Ecrans_id.Video != null) {
+            } else if (this.current.Ecrans_id.Type == "video") {
               this.compiled += `
                                     <video controls width="500" autoplay='true'>
 
@@ -159,18 +196,17 @@ export default {
 
                                         Sorry, your browser doesn't support embedded videos.
                                     </video>
-                                  </div>
-                                  `;
+                                  </div>`;
             }
           } else {
-            if (this.current.Ecrans_id.Image != null) {
+            if (this.current.Ecrans_id.Type == "image") {
               this.compiled += `<div id='imgFull' style='background-position: center;width: 100vw; height: 100vh;background-size: cover;background-image: url(https://api.interdisp.valentinbardet.dev/assets/${this.current.Ecrans_id.Image})' /></div>`;
               // this.compiled = `<img src='http://149.91.80.75:8055/assets/${this.current.Ecrans_id.Image}' />`;
-            } else if (this.current.Ecrans_id.Markdown != null) {
+            } else if (this.current.Ecrans_id.Type == "texte") {
               this.compiled += "<section class='texte'>";
               this.compiled += marked.parse(this.current.Ecrans_id.Markdown);
               this.compiled += "</section>";
-            } else if (this.current.Ecrans_id.Video != null) {
+            } else if (this.current.Ecrans_id.Type == "video") {
               this.compiled += `
                                       <video controls width="500" autoplay='true'>
 
@@ -178,6 +214,115 @@ export default {
 
                                         Sorry, your browser doesn't support embedded videos.
                                     </video>`;
+            } else if (this.current.Ecrans_id.Type == "meteo") {
+              console.log("test");
+              this.compiled += `<style> :root {
+    --bg_main: #0a1f44;
+    --text_light: #fff;
+    --text_med: #53627c;
+    --text_dark: #1e2432;
+    --red: #ff1e42;
+    --darkred: #c3112d;
+    --orange: #ff8c00;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-weight: normal;
+  }
+
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+
+
+  body {
+    font: 1rem/1.3 "Roboto", sans-serif;
+    background: var(--bg_main);
+    color: var(--text_dark);
+  }
+
+  .container {
+    width: 100%;
+    max-width: 1080px;
+    margin: 0 auto;
+    padding: 0 15px;
+  }
+
+
+
+
+  /* SECTION #2
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+  .ajax-section {
+    padding: 70px 0 20px;
+  }
+
+
+
+  .ajax-section .city {
+    position: relative;
+    padding: 40px 10%;
+    border-radius: 20px;
+    background: var(--text_light);
+    color: var(--text_med);
+
+  }
+
+  .ajax-section .city::after {
+    content: '';
+    width: 90%;
+    height: 50px;
+    position: absolute;
+    bottom: -12px;
+    left: 5%;
+    z-index: -1;
+    opacity: 0.3;
+    border-radius: 20px;
+    background: var(--text_light);
+  }
+
+
+  .ajax-section .city-temp {
+    font-size: 5rem;
+    font-weight: bold;
+    margin-top: 10px;
+    color: var(--text_dark);
+  }
+
+  .ajax-section .city sup {
+    font-size: 0.5em;
+  }
+
+  .ajax-section .city-name sup {
+    padding: 0.2em 0.6em;
+    border-radius: 30px;
+    color: var(--text_light);
+    background: var(--orange);
+  }
+
+  .ajax-section .city-icon {
+    margin-top: 10px;
+    width: 100px;
+    height: 100px;
+  }</style>
+                                <div onload="${this.meteo(
+                                  "Nancy",
+                                  "4d8fb5b93d4af21d66a2948710284366"
+                                )}">
+                                  <section class="ajax-section">
+                                    <div class="container">
+                                      <div class="cities"></div>
+                                    </div>
+                                  </section>
+                                                
+                                </div>`;
             }
           }
         }
